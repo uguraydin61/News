56998f03b3ed4c7ba36f9f59dafb81b1c807cd03
uguraydin61 <bmuguraydin@gmail.com>
2019-03-12 11:01:58 +0300

    Kurulum

--- News/News/Controllers/HomeController.cs
+++ News/News/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace News.Controllers
         {
             return View();
         }
-
+        
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
