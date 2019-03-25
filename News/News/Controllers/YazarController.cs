54056b91ef24792f9870120b2df251245fdd60dc
uguraydin61 <bmuguraydin@gmail.com>
2019-03-25 14:54:20 +0300

    Kurulum

---
+++ News/News/Controllers/YazarController.cs
@@ -0,0 +1,25 @@
+﻿using BLL;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace News.Controllers
+{
+    public class YazarController : Controller
+    {
+        // GET: Yazar
+        UnitOfWork _uw = new UnitOfWork();
+
+        public ActionResult Liste()
+        {
+
+            List<Authors> AuthorList = _uw.AuthorsRep.BringItAll();
+
+            return View(AuthorList);
+
+        }
+    }
+}
